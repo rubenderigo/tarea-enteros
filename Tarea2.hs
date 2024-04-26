51930de645a2a7d7cf89b5cd9963cefa5e4bec65
Ruben Derigo <rm.derigo@gmail.com>
2024-04-25 23:01:05 -0300

    add solution for points 2.1 and 2.2

--- Tarea2.hs
+++ Tarea2.hs
@@ -76,11 +76,20 @@ suma_i n = suma_entre_f (\x -> x) n 0
 
 --1.
 es_divisor :: N -> N -> Bool
-es_divisor n k = undefined
+es_divisor n k
+	| n == k || k == 0 = True
+	| n < k = False
+	| n > k = es_divisor (n-k) k
 
 --2.
 primer_divisor :: N -> N
-primer_divisor n = undefined
+primer_divisor n =
+	let buscarDivisor k
+			| k > n = n
+			| es_divisor n k = k
+			| otherwise = buscarDivisor (k + 1)
+	in buscarDivisor 2
+ 
 
 --3.
 es_primo :: N -> Bool
